6e9f729301d7270beb93eda4a451970667dfa04d
thevpc <taha.bensalah@gmail.com>
2025-08-20 01:50:24 +0100

    Update module-info.java : fixed to support j9+ module system

---
+++ pangaea-note-plus/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module net.thevpc.pnoteplus {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires com.jfoenix;
+    requires net.thevpc.nuts;
+
+    exports net.thevpc.pnoteplus;
+    exports net.thevpc.pnoteplus.frame;
+}
